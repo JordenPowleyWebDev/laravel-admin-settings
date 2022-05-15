d41d0d68a9aacdb41b7711e6b942e4a3f703302b
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2022-05-15 16:24:26 +0100

    Add Initial Settings.

---
+++ readme.md
@@ -0,0 +1,5 @@
+# laravel-admin-settings
+A  basic library for key => value admin settings
+
+## Installation
+`composer require jordenpowleywebdev/laravel-admin-settings`
---
+++ src/LaravelAdminSettingsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace JordenPowleyWebDev\LaravelAdminSettings;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAdminSettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * LaravelAdminSettingsServiceProvider::register()
+     */
+    public function register()
+    {
+        // Merge in the package config
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-admin-settings.php', 'laravel-admin-settings');
+    }
+
+    /**
+     * LaravelAdminSettingsServiceProvider::boot()
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+
+            // Make Config Files Available To The App
+            $this->publishes([
+                __DIR__.'/../config/laravel-admin-settings.php' => config_path('laravel-admin-settings.php'),
+            ], 'config');
+        }
+    }
+}
